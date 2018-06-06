6dace2442d8e3bff6ea31427fab676b269c05296
Prateek <prateekcelly@gmail.com>
2018-06-06 23:22:59 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,80 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import mongoose from 'mongoose';
+import Comment from './models/comment';
+
+const app = express();
+const router = express.Router();
+
+const API_PORT = process.env.API_PORT || 3001;
+
+var mongoDB = 'mongodb://127.0.0.1/mern-box';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db=mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+router.put('/comments/:commentId', (req, res) => {
+  const { commentId } = req.params;
+  if (!commentId) {
+    return res.json({ success: false, error: 'No comment id provided' });
+  }
+  Comment.findById(commentId, (error, comment) => {
+    if (error) return res.json({ success: false, error });
+    const { author, text } = req.body;
+    if (author) comment.author = author;
+    if (text) comment.text = text;
+    comment.save(error => {
+      if (error) return res.json({ success: false, error });
+      return res.json({ success: true });
+    });
+  });
+});
+
+router.delete('/comments/:commentId', (req, res) => {
+  const { commentId } = req.params;
+  if (!commentId) {
+    return res.json({ success: false, error: 'No comment id provided' });
+  }
+  Comment.remove({ _id: commentId }, (error, comment) => {
+    if (error) return res.json({ success: false, error });
+    return res.json({ success: true });
+  });
+});
+
+router.get('/comments', (req, res) => {
+  Comment.find((err, comments) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: comments });
+  });
+});
+
+router.post('/comments', (req, res) => {
+  const comment = new Comment();
+  const { author, text } = req.body;
+  if (!author || !text) {
+    return res.json({
+      success: false,
+      error: 'You must provide an author and comment'
+    });
+  }
+  comment.author = author;
+  comment.text = text;
+  comment.save(err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+router.get('/', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+app.use('/api', router);
+
+app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
---
+++ client/src/CommentBox.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import 'whatwg-fetch';
+import CommentList from './CommentList';
+import CommentForm from './CommentForm';
+import './CommentBox.css';
+
+class CommentBox extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      error : null,
+      author : '',
+      text : '',
+    };
+    this.pollInterval = null;
+  }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    if (!this.pollInterval) {
+      this.pollInterval = setInterval(this.loadCommentsFromServer, 2000);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.pollInterval) clearInterval(this.pollInterval);
+    this.pollInterval = null;
+  }
+
+  loadCommentsFromServer = () => {
+    fetch('/api/comments/')
+      .then(data => data.json())
+      .then((res) => {
+        if (!res.success) this.setState({ error: res.error });
+        else this.setState({ data: res.data });
+      });
+  }
+
+  onChangeText = (e) => {
+  const newState = { ...this.state };
+  newState[e.target.name] = e.target.value;
+  this.setState(newState);
+}
+
+submitComment = (e) => {
+    e.preventDefault();
+    const { author, text } = this.state;
+    if (!author || !text) return;
+    fetch('/api/comments', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ author, text }),
+    }).then(res => res.json()).then((res) => {
+      if (!res.success) this.setState({ error: res.error.message || res.error });
+      else this.setState({ author: '', text: '', error: null });
+    });
+  }
+
+  render() {
+      return (
+      <div className="container">
+        <div className="comments">
+          <h2>Comments:</h2>
+          <CommentList data={this.state.data} />
+        </div>
+        <div className="form">
+          <CommentForm
+           author={this.state.author}
+           text={this.state.text}
+           handleChangeText={this.onChangeText}
+           handleSubmit={this.submitComment} />
+        </div>
+        {this.state.error && <p>{this.state.error}</p>}
+      </div>
+    );
+  }
+}
+
+export default CommentBox;
